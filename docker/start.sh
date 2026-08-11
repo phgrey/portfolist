b40dd4cb8bf0b1c28f7948609fc2ea1ff612e158
Sergey Semenov <phgrey@gmail.com>
2026-08-11 18:35:41 +0200

    dockerization is done

--- docker/start.sh
+++ docker/start.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# 1. Start Tailscale daemon in userspace mode
-/app/tailscaled --tunedev=socks5 --socket=/var/run/tailscale/tailscaled.sock &
+tailscaled --state=/var/lib/tailscale/tailscaled.state \
+  --socket=/var/run/tailscale/tailscaled.sock &
 
-# 2. Wait briefly and log in using your injected Auth Key env variable
-/app/tailscale --socket=/var/run/tailscale/tailscaled.sock up \
-  --authkey="${TAILSCALE_AUTHKEY}" \
-  --hostname="firebase-node-app"
+sleep 2
+if [ -n "$TAILSCALE_AUTHKEY" ]; then
+  tailscale up --authkey="${TAILSCALE_AUTHKEY}" \
+    --hostname="portfolist" --accept-routes
+fi
+tailscale funnel --bg 3000
 
-/app/tailscale serve 3000 \
-  --service=svc:portfolist \
-  --authkey="${TAILSCALE_AUTHKEY}"
-
-# 3. Boot your native Node.js application
 node dist/server.cjs
+
